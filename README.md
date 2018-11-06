6cdc24242311f2078d88c7c1392b9966bccc2985
yasarertekin <yasarertekin@outlook.com>
2018-11-06 14:00:48 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# lambda-deployment-example
+
+An example of how to deploy a Lambda function automatically using CodePipeline and Terraform.
+
+Discussion on my site [here](https://theconsultingcto.com/posts/automated-lambda-deployments-with-terraform-codepipeline/).
---
+++ pipeline/main.tf
@@ -0,0 +1,76 @@
+data "aws_region" "current" {
+  current = true
+}
+
+# Insert any remote state here
+
+module "dev" {
+  source = "./dev"
+  region = "${data.aws_region.current.name}"
+
+  assume_role_arn = ""
+
+  function_name = "lambda-deployment-example"
+  function_arn  = ""
+  function_jar  = "lambda-deployment-example.jar"
+
+  source_github_token = ""
+  source_owner        = ""
+  source_repo         = ""
+  source_branch       = ""
+
+  codepipeline_service_role_arn  = ""
+  lambda_build_artifacts_bucket  = ""
+  lambda_build_artifacts_key_arn = ""
+  lambda_builder_project_name    = ""
+
+  remote_state_bucket            = ""
+  remote_state_bucket_arn        = ""
+  remote_state_kms_key_arn       = ""
+  remote_state_locking_table_arn = ""
+  remote_state_region            = ""
+}
+
+module "stage" {
+  source = "./stage"
+  region = ""
+
+  assume_role_arn = ""
+
+  function_name = "lambda-deployment-example"
+  function_arn  = ""
+  function_jar  = "lambda-deployment-example.jar"
+
+  codepipeline_service_role_arn  = ""
+  lambda_build_artifacts_key_arn = ""
+  lambda_build_artifacts_bucket  = ""
+  lambda_builder_project_name    = ""
+
+  remote_state_bucket            = ""
+  remote_state_bucket_arn        = ""
+  remote_state_kms_key_arn       = ""
+  remote_state_locking_table_arn = ""
+  remote_state_region            = ""
+}
+
+module "prod" {
+  source = "./prod"
+  region = ""
+
+  assume_role_arn = ""
+
+  function_name = "lambda-deployment-example"
+  function_arn  = ""
+  function_jar  = "lambda-deployment-example.jar"
+
+  codepipeline_service_role_arn  = ""
+  lambda_build_artifacts_key_arn = ""
+  lambda_build_artifacts_bucket  = ""
+  lambda_builder_project_name    = ""
+
+  remote_state_bucket            = ""
+  remote_state_bucket_arn        = ""
+  remote_state_kms_key_arn       = ""
+  remote_state_locking_table_arn = ""
+  remote_state_region            = ""
+}
